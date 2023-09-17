74dbf909f0a1a87a67cf48015c28746780cebe1a
TrHau03 <haunhoooo@gmail.com>
2023-09-17 18:47:47 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+
+   MongooseModule.forRoot('mongodb+srv://admin:haunho@cluster0.3oqlz8r.mongodb.net/TWStore?retryWrites=true&w=majority'),
+   UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/dto/user_insert_response.ts
@@ -0,0 +1,5 @@
+export class UserInsertResponseDTO{
+    status: Boolean;
+    message: string;
+}
+
